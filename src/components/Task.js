10a6f3dd3c9808b3d30a2d0e72540890ff4cc709
Chiara Del Rio <Chiara.del.rio1@ibm.com@chiaras-mbp.sbank.uk.ibm.com>
2019-07-30 17:19:03 +0100

    work through tutorial up addons section

--- src/components/Task.js
+++ src/components/Task.js
@@ -14,8 +14,13 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
       <div className="title">
-        <input type="text" value={title} readOnly={true} placeholder="Input title" />
-      </div>
+      <input
+  type="text"
+  value={title}
+  readOnly={true}
+  placeholder="Input title"
+  style={{ textOverflow: 'ellipsis' }}
+/>      </div>
 
       <div className="actions" onClick={event => event.stopPropagation()}>
         {state !== 'TASK_ARCHIVED' && (
@@ -28,7 +33,7 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
   );
 }
 
-Task.propTypes = {
+ Task.propTypes = {
     task: PropTypes.shape({
       id: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
